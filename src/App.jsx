9bd33d90323411a2dc556da33976b4fc6a2f1bd5
Elise Chant <elise.chant@gmail.com>
2021-04-17 15:00:34 +1000

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,18 @@ export function App() {
 
   return (
     <div style={{ height: '100%', width: '100%', backgroundColor: '#FEFEFE' }}>
-      <div className={'container max-w-3xl sm-container mx-auto py-16 px-4 flex flex-col h-full'}>
+      <div className={'container max-w-3xl sm-container mx-auto py-8 md:py-16 px-4 flex flex-col h-full'}>
         {(!shownSplash && location.pathname === '/') && <Splash onComplete={() => setShownSplash(true)} />}
         <div className={'flex'}>
-          <div><img src={Logo} width="160" alt="Bee Stripes" /></div>
+          <div className={'w-2/6 md:w-min'}><img src={Logo} width="160" alt="Bee Stripes" /></div>
         </div>
-        <div className={'flex flex-1 items-center'}>
+        <div className={'flex flex-1 md:items-center'}>
           <Switch>
             <Route key={'home'} path={'/'} component={() => <Home onEnter={shownSplash} />} />
             <Route key={'about'} path={'/about'} component={About} />
           </Switch>
         </div>
-        <div className={'absolute bottom-4 left-6'}>
+        <div className={'md:absolute md:bottom-4 md:left-6 '}>
           <p className="mt-2 pb-6 leading-normal" style={{color: '#b4b4b4'}}>hi@beestripes.studio</p>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,16 +22,16 @@ const Home = ({ onEnter }) => {
 
   return (
     <motion.div {...animationProps} className={'block font-medium'}>
-      <h2 className="text-6xl text-black" style={{ fontFamily: tokens.fontFamily.jungkaMedium }}>
+      <h2 className="pt-16 md:pt-0 text-5xl md:text-6xl text-black" style={{ fontFamily: tokens.fontFamily.jungkaMedium }}>
       <span className={'block'}>
         <span style={{ color: 'red' }}>_we</span> build products that
       </span>
         <span className={'block'}>test ideas</span>
       </h2>
-      <div className={'mt-20 text-black'} style={{ fontFamily: tokens.fontFamily.jungkaLight }}>
-        <p className="text-2xl pb-6 leading-normal">We swarm with you to test and validate your business ideas.</p>
-        <p className="text-2xl pb-6 leading-normal">We can build the tech to take your product to market – iOS and Android apps, web sites, web services or web apps. Other ideas? challenge us.</p>
-        <p className="text-2xl pb-6 leading-normal">We know starting a business is a leap and can be lonely. <span style={{ fontFamily: tokens.fontFamily.jungkaMedium }}>Let's startup together!</span></p>
+      <div className={'mt-14 md:mt-20 text-black'} style={{ fontFamily: tokens.fontFamily.jungkaLight }}>
+        <p className="text-xl md:text-2xl pb-6 leading-normal">We swarm with you to test and validate your business ideas.</p>
+        <p className="text-xl md:text-2xl pb-6 leading-normal">We can build the tech to take your product to market – iOS and Android apps, web sites, web services or web apps. Other ideas? challenge us.</p>
+        <p className="text-xl md:text-2xl pb-6 leading-normal">We know starting a business is a leap and can be lonely. <span style={{ fontFamily: tokens.fontFamily.jungkaMedium }}>Let's startup together!</span></p>
       </div>
     </motion.div>
   )
